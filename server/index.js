253b100424e796b98cf54929871fd9a59d5db839
wilson <wilsonlmk23@gmail.com>
2024-06-11 16:13:09 +0800

    chore: setup ESLint and Prettier

--- server/index.js
+++ server/index.js
@@ -1,16 +1,16 @@
-import 'dotenv/config';
-import express from 'express';
+import "dotenv/config";
+import express from "express";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Crypto Trading Platform!');
+app.get("/", (req, res) => {
+  res.send("Hello, Crypto Trading Platform!");
 });
 
 app.listen(PORT, (err) => {
   if (err) {
-    console.error('Failed to start server:', err);
+    console.error("Failed to start server:", err);
   } else {
     console.log(`Server is running on port ${PORT}`);
   }
