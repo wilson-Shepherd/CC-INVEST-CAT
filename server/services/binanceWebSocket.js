f11a4001cb250a0031a2c3ad273fad54b791f3f4
wilson <wilsonlmk23@gmail.com>
2024-06-13 13:51:52 +0800

    modify: update WebSocket crawler using binance-api-node

--- server/services/binanceWebSocket.js
+++ server/services/binanceWebSocket.js
@@ -1,21 +1,21 @@
-import WebSocket from 'ws';
+import Binance from 'binance-api-node';
 import { EventEmitter } from 'events';
 import dotenv from 'dotenv';
-import axios from 'axios';
 
 dotenv.config();
 
 class BinanceWebSocket extends EventEmitter {
   constructor() {
     super();
+    this.client = Binance();
     this.symbols = [];
     this.connect();
   }
 
   async fetchSymbols() {
     try {
-      const response = await axios.get('https://api.binance.com/api/v3/exchangeInfo');
-      const symbols = response.data.symbols.filter(symbol => symbol.quoteAsset === 'USDT');
+      const exchangeInfo = await this.client.exchangeInfo();
+      const symbols = exchangeInfo.symbols.filter(symbol => symbol.quoteAsset === 'USDT');
       this.symbols = symbols.map(symbol => symbol.symbol.toLowerCase());
     } catch (error) {
       console.error('Error fetching symbols:', error);
@@ -25,27 +25,12 @@ class BinanceWebSocket extends EventEmitter {
   async connect() {
     await this.fetchSymbols();
     const streams = this.symbols.map(symbol => `${symbol}@ticker`).join('/');
-    const socketUrl = `wss://stream.binance.com:9443/ws/${streams}`;
-    this.ws = new WebSocket(socketUrl);
-
-    this.ws.on('open', () => {
-      console.log(`Binance WebSocket API connected`)
+    
+    this.client.ws.customSubStream(streams, data => {
+      this.emit('tickerData', data);
     });
 
-    this.ws.on('message', (data) => {
-      const message = data.toString('utf-8');
-      const jsonMessage = JSON.parse(message);
-      this.emit('tickerData', jsonMessage);
-    });
-
-    this.ws.on('error', (error) => {
-      console.error('WebSocket error:', error);
-    });
-
-    this.ws.on('close', () => {
-      console.log('WebSocket connection closed, reconnecting...');
-      setTimeout(() => this.connect(), 1000);
-    });
+    console.log('Binance WebSocket API connected');
   }
 }
 
